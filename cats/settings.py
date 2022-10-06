f1538157d020cf24db32ae3fe63f4cda433690d7
NathCoursey <ncoursey314@gmail.com>
2022-10-06 14:14:36 -0400

    Config deployment

--- cats/settings.py
+++ cats/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import os
 
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/artists/'
 
 LOGOUT_REDIRECT_URL = '/'
+
 
